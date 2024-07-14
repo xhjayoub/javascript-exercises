f8a29d3d9df27b0f05c9d029761efb2d9e706a70
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-14 11:11:17 +0100

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number' || a<0 || b<0) {
+        return "ERROR";
+    }
+    if (b<a) {
+        let z = a;
+        a = b;
+        b = z;
+    }
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
