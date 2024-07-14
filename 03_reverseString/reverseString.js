9cc1a8dc6a86cc39081a08c1ea76f9404c0c8e40
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-14 10:30:03 +0100

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let ch = '';
+    for (let i = 0; i < string.length; i++) {
+        ch += string[string.length - i - 1];
+    }
+    return ch;
 };
 
 // Do not edit below this line
