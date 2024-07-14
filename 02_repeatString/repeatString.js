31ecc96315c12a0ac63c2d4e465cd4496ec2c4eb
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-14 10:25:28 +0100

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    let ch = '';
+    for (let i = 0; i<num; i++) {
+        ch += string;
+    }
+    return ch
 };
 
 // Do not edit below this line
