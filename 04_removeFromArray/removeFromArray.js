cd6ed007a46a417581e4b66305078b90613bb693
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-14 11:01:50 +0100

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let arr2 = [];
+    let n = 0;
+    for (let i = 0; i< arr.length; i++) {
+        let v = false;
+        for (let k = 1; k < arguments.length; k++) {
+            if (arr[i] === arguments[k]) {
+                v = true;
+                break;
+            }
+        }
+        if (!v) {
+            arr2[n] = arr[i];
+            n++;
+        }
+    }
+
+    return arr2;
 };
 
 // Do not edit below this line
