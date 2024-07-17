0cd23b1067669d68f9dcd734ba664d9345da6654
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-17 15:56:08 +0100

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+function stringCleaner(string) {
+    let cleanString = '';
+    for (let i = 0; i< string.length; i++) {
+        if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=122 || string[i]<='9' && string[i]>='0') {
+            cleanString += string[i];
+        }
+    }
+    return cleanString.toLowerCase();
+}
 
+const palindromes = function (string) {
+    let reverse = '';
+    for (let i = 0; i < string.length; i++) {
+        reverse += string[string.length-i-1];
+    }
+    return stringCleaner(reverse) == stringCleaner(string);
 };
 
 // Do not edit below this line
