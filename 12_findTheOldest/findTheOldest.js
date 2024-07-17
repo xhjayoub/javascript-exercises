3a8ef3d3c13c1c8cff45f8b0bf94fada11e893aa
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-17 16:25:42 +0100

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+function calcAge(Death, Birth) {
+    if (Death === undefined) {
+        let d = new Date();
+        Death = d.getFullYear();
+    }
+    return Death - Birth;
+}
 
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    for (let i = 1; i< people.length ; i++) {
+        if (calcAge(people[i].yearOfDeath,people[i].yearOfBirth) > calcAge(oldest.yearOfDeath, oldest.yearOfBirth)) {
+            oldest = people[i];
+        }
+
+    }
+    return oldest;
 };
 
 // Do not edit below this line
