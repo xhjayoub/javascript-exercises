2412aa32881a260b8aa53351dc8a1c49b67c4629
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-17 16:02:33 +0100

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0) {
+        return 'OOPS';
+    }
+    if (n===0) {
+        return 0;
+    }
+    let fib = [1, 1];
+    let i = 1;
+    while (i !== n) {
+        let z = fib[0];
+        fib[0] = fib[1];
+        fib[1] = z + fib[1];
+        i++;
+    }
+    return fib[0];
 };
 
 // Do not edit below this line
