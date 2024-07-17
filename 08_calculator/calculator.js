4a8f51d2100e5a5afcd146abb69d85a5a42f0faf
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-17 15:34:44 +0100

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, num) => {
+    return sum + num;
+  },0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((sum, num) => {
+    return sum*num;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let sum = 1;
+  for (let i = 1; i<=n; i++) {
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
